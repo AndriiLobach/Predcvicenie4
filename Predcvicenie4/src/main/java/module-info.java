5b8c03de6ea9b6891585fbec1193e027dfc030c1
PC1 <xlobach@stuba.sk>
2023-03-16 16:09:40 +0100

    precvicene

---
+++ Predcvicenie4/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sk.stuba.fei.oop.predcvicenie4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens sk.stuba.fei.oop.predcvicenie4 to javafx.fxml;
+    exports sk.stuba.fei.oop.predcvicenie4;
+}
